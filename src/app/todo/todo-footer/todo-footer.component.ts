68c1b8b3e8077c5151e488482b2f25867104d79b
Sebastian <sebastian.valencian@gmail.com>
2019-10-09 18:19:18 -0500

    [Feat]Modificar el filtro del footer

--- src/app/todo/todo-footer/todo-footer.component.ts
+++ src/app/todo/todo-footer/todo-footer.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as fromFiltro from '../../filter/filter.actions';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducers';
+
 @Component({
   selector: 'app-todo-footer',
   templateUrl: './todo-footer.component.html',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoFooterComponent implements OnInit {
 
-  constructor() { }
+
+  filtrosValidos: fromFiltro.filtrosValidos[] = ['todos', 'completados', 'pendientes'];
+  filtroActual: fromFiltro.filtrosValidos;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.store.subscribe( state =>{
+      this.filtroActual = state.filtro;
+    });
+  }
+
+  cambiarFiltro(filtro: fromFiltro.filtrosValidos) {
+
+    const accion = new fromFiltro.SetFiltroAction(filtro);
+    this.store.dispatch(accion);
   }
 
 }
